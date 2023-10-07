c014c7d23c9053d16d0edc59bff1a6a4fbba13f0
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-10-07 20:22:13 +0200

    Destructure data in api

--- src/core/services/api.repository.ts
+++ src/core/services/api.repository.ts
@@ -4,7 +4,7 @@ export class ApiRepository<T extends { id: string | number }> {
   constructor(public url: string) {}
 
   async fetchAll(): Promise<T[]> {
-    const response = await axios.get(this.url);
-    return response.data;
+    const { data } = await axios.get(this.url);
+    return data;
   }
 }
