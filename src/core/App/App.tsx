8e9713e5465df30b98eac61cbc9bccfc511d1260
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-10-07 18:41:31 +0200

    Add json server dependency

--- src/core/App/App.tsx
+++ src/core/App/App.tsx
@@ -1,13 +1,14 @@
-import {Sidebar} from "../../layout/sidebar/sidebar.tsx";
-import {Navbar} from "../../layout/navbar/navbar.tsx";
+import { Sidebar } from "../../layout/sidebar/sidebar.tsx";
+import { Navbar } from "../../layout/navbar/navbar.tsx";
 import { List } from "../../marketplace/components/list/list";
 export function App() {
   return (
     <div className="flex flex-row h-full min-h-screen overflow-y-hidden bg-slate-800">
       <div className="w-full">
-        <Navbar/>
+        <Navbar />
+        <List />
       </div>
-      <Sidebar/>
+      <Sidebar />
     </div>
-  )
+  );
 }
--- src/layout/sidebar/sidebar.tsx
+++ src/layout/sidebar/sidebar.tsx
@@ -1,9 +1,5 @@
-
 export function Sidebar() {
-
   return (
-    <div className="w-2/12 bg-[conic-gradient(at_bottom,_var(--tw-gradient-stops))] from-slate-500 via-gray-900 to-slate-500 text-center border-r-2 border-slate-300 text-slate-200 flex flex-col justify-between ">
-
-    </div>
+    <div className="w-2/12 bg-[conic-gradient(at_bottom,_var(--tw-gradient-stops))] from-slate-500 via-gray-900 to-slate-500 text-center border-r-2 border-slate-300 text-slate-200 flex flex-col justify-between "></div>
   );
-};
+}
