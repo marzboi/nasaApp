dc87f8c19e612d67e12843a9d51ce1d60cd03c90
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-10-07 19:18:52 +0200

    Add modal to card List component

--- src/marketplace/components/project-card/project-card.tsx
+++ src/marketplace/components/project-card/project-card.tsx
@@ -1,23 +1,69 @@
+import { useState } from "react";
 import { Project } from "../../models/data";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
 
 type PropsType = {
   item: Project;
 };
 
 export function ProjectCard({ item }: PropsType) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [size, setSize] = React.useState("md");
+
+  const handleSizeClick = (newSize: React.SetStateAction<string>) => {
+    setSize(newSize);
+    onOpen();
+  };
+
   return (
     <>
-      <li key={item.id} className="container text-center">
+      <li key={item.id} className="container-sm text-center">
         <div>
           <h2>{item.name}</h2>
           <img src={item.img} alt="" className="rounded mx-auto d-block" />
-          <p>{item.description}</p>
-          <p>
-            {item.author.firstName} {item.author.lastName}
-          </p>
+          <div>
+            <p>{item.description}</p>
+            <p>
+              {item.author.firstName} {item.author.lastName}
+            </p>
+          </div>
         </div>
-        <button className="btn btn-success">View</button>
+        <button
+          className="btn btn-success"
+          onClick={() => handleSizeClick("full")}
+        >
+          View
+        </button>
       </li>
+      <>
+        <Modal onClose={onClose} size={size} isOpen={isOpen}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>{item.name}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <p>{item.description}</p>
+              <p>
+                {item.author.firstName} {item.author.lastName}
+              </p>
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={onClose}>Close</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
     </>
   );
 }
