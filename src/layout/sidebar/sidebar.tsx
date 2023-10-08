5075fccdeba0593cf17c2ad0b8e587b628e0e259
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-10-08 12:56:21 +0200

    Remove undeclared var

--- src/layout/sidebar/sidebar.tsx
+++ src/layout/sidebar/sidebar.tsx
@@ -1,34 +1,35 @@
-import React from 'react'
 import {
   Box,
   useColorModeValue,
   Drawer,
   DrawerContent,
   useDisclosure,
-
-} from '@chakra-ui/react'
-import {MobileNav} from "./MobileNav.tsx";
-import {SidebarContent} from "./SidebarContent.tsx";
-
+} from "@chakra-ui/react";
+import { MobileNav } from "./MobileNav.tsx";
+import { SidebarContent } from "./SidebarContent.tsx";
 
 export function Sidebar() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
-      <SidebarContent onClose={() => onClose} display={{ base: 'none', md: 'block' }} />
+    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
+      <SidebarContent
+        onClose={() => onClose}
+        display={{ base: "none", md: "block" }}
+      />
       <Drawer
         isOpen={isOpen}
         placement="left"
         onClose={onClose}
         returnFocusOnClose={false}
         onOverlayClick={onClose}
-        size="full">
+        size="full"
+      >
         <DrawerContent>
           <SidebarContent onClose={onClose} />
         </DrawerContent>
       </Drawer>
-      <MobileNav display={{ base: 'flex', md: 'none' }} onOpen={onOpen} />
-      <Box ml={{ base: 0, md: 60 }} p="4"/>
+      <MobileNav display={{ base: "flex", md: "none" }} onOpen={onOpen} />
+      <Box ml={{ base: 0, md: 60 }} p="4" />
     </Box>
-  )
+  );
 }
