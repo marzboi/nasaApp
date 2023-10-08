f451f8889f370e066e1a4a8af96c8c67a74f6085
daasheo <jesusmudev@gmail.com>
2023-10-08 12:35:53 +0200

    Upload sidebar_ui

---
+++ src/layout/sidebar/MobileNav.tsx
@@ -0,0 +1,25 @@
+import {Flex, IconButton, useColorModeValue} from "@chakra-ui/react";
+import {FiMenu} from "react-icons/fi";
+import React from "react";
+
+export const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+  return (
+    <Flex
+      ml={{ base: 0, md: 60 }}
+      px={{ base: 4, md: 24 }}
+      height="20"
+      alignItems="center"
+      bg={useColorModeValue('white', 'gray.900')}
+      borderBottomWidth="1px"
+      borderBottomColor={useColorModeValue('gray.200', 'gray.900')}
+      justifyContent="flex-start"
+      {...rest}>
+      <IconButton
+        variant="outline"
+        onClick={onOpen}
+        aria-label="open menu"
+        icon={<FiMenu />}
+      />
+    </Flex>
+  )
+}
---
+++ src/layout/sidebar/SidebarContent.tsx
@@ -0,0 +1,42 @@
+import {Box, BoxProps, CloseButton, Flex, useColorModeValue} from "@chakra-ui/react";
+import {NavItem} from "./NavItem.tsx";
+import React from "react";
+import {FiCompass, FiHome, FiSettings, FiStar, FiTrendingUp} from "react-icons/fi";
+import {IconType} from "react-icons";
+
+interface LinkItemProps {
+  name: string
+  icon: IconType
+}
+
+const LinkItems: Array<LinkItemProps> = [
+  { name: 'Home', icon: FiHome },
+  { name: 'Trending', icon: FiTrendingUp },
+  { name: 'Explore', icon: FiCompass },
+  { name: 'Favourites', icon: FiStar },
+  { name: 'Settings', icon: FiSettings },
+]
+interface SidebarProps extends BoxProps {
+  onClose: () => void
+}
+export const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
+  return (
+    <Box
+      bg={useColorModeValue('white', 'gray.900')}
+      borderRight="1px"
+      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
+      w={{ base: 'full', md: 60 }}
+      pos="fixed"
+      h="full"
+      {...rest}>
+      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+        <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+      </Flex>
+      {LinkItems.map((link) => (
+        <NavItem key={link.name} icon={link.icon}>
+          {link.name}
+        </NavItem>
+      ))}
+    </Box>
+  )
+}
