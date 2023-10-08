b32a6a7831bc0de5deadb834d35e970d96e73bda
daasheo <jesusmudev@gmail.com>
2023-10-08 10:06:00 +0200

    New sidebar and search-bar component.

--- src/layout/sidebar/sidebar.tsx
+++ src/layout/sidebar/sidebar.tsx
@@ -1,8 +1,6 @@
-import { Box, Link, Text } from '@chakra-ui/react';
-
+import { Box, Text, Link, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 
 export function Sidebar() {
-
   return (
     <Box
       width="250px"
@@ -13,31 +11,52 @@ export function Sidebar() {
       flexDirection="column"
       p="4"
     >
-      <Text fontSize="xl" fontWeight="bold" mb="4">
-        My Sidebar
-      </Text>
-      <Box>
+      <Menu>
         <Text fontSize="lg" fontWeight="bold" mb="2">
           My Projects
         </Text>
-        <Link href="#">Project 1</Link>
-        <Link href="#">Project 2</Link>
-        <Link href="#">Project 3</Link>
-      </Box>
+        <MenuButton as={Link} color="blue.500" cursor="pointer">
+          Project Menu
+        </MenuButton>
+        <MenuList>
+          <MenuItem as={Link} href="#">
+            Project 1
+          </MenuItem>
+          <MenuItem as={Link} href="#">
+            Project 2
+          </MenuItem>
+          <MenuItem as={Link} href="#">
+            Project 3
+          </MenuItem>
+        </MenuList>
+      </Menu>
+
       <Box mt="4">
         <Text fontSize="lg" fontWeight="bold" mb="2">
           Last Project Seen
         </Text>
         <Link href="#">Last Project</Link>
       </Box>
-      <Box mt="4">
-        <Text fontSize="lg" fontWeight="bold" mb="2">
+
+      <Menu>
+        <Text fontSize="lg" fontWeight="bold" mt="4" mb="2">
           Partners
         </Text>
-        <Link href="#">Partner 1</Link>
-        <Link href="#">Partner 2</Link>
-        <Link href="#">Partner 3</Link>
-      </Box>
+        <MenuButton as={Link} color="blue.500" cursor="pointer">
+          Partners Menu
+        </MenuButton>
+        <MenuList>
+          <MenuItem as={Link} href="#">
+            Partner 1
+          </MenuItem>
+          <MenuItem as={Link} href="#">
+            Partner 2
+          </MenuItem>
+          <MenuItem as={Link} href="#">
+            Partner 3
+          </MenuItem>
+        </MenuList>
+      </Menu>
     </Box>
   );
-};
+}
