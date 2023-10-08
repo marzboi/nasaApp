e10a92f55476c7cbc801788b6d7aac20bfe552e9
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-10-08 09:51:43 +0200

    Saving local and implement profile photos

--- src/layout/navbar/navbar.tsx
+++ src/layout/navbar/navbar.tsx
@@ -4,14 +4,12 @@ import {
   Box,
   Flex,
   Avatar,
-  Text,
   Button,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
   MenuDivider,
-  useDisclosure,
   useColorModeValue,
   Stack,
   useColorMode,
@@ -19,33 +17,8 @@ import {
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
-interface Props {
-  children: React.ReactNode;
-}
-
-const NavLink = (props: Props) => {
-  const { children } = props;
-
-  return (
-    <Box
-      as="a"
-      px={2}
-      py={1}
-      rounded={"md"}
-      _hover={{
-        textDecoration: "none",
-        bg: useColorModeValue("gray.200", "gray.700"),
-      }}
-      href={"#"}
-    >
-      {children}
-    </Box>
-  );
-};
-
 export function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
-  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
@@ -66,22 +39,16 @@ export function Navbar() {
                   cursor={"pointer"}
                   minW={0}
                 >
-                  <Avatar
-                    size={"sm"}
-                    src={"https://bit.ly/sage-adebayo"}
-                  />
+                  <Avatar size={"sm"} src={"./profile.jpeg"} />
                 </MenuButton>
                 <MenuList alignItems={"center"}>
                   <br />
                   <Center>
-                    <Avatar
-                      size={"2xl"}
-                      src={"https://bit.ly/sage-adebayo"}
-                    />
+                    <Avatar size={"2xl"} src={"./profile.jpeg"} />
                   </Center>
                   <br />
                   <Center>
-                    <p>Username</p>
+                    <p>Mr. President</p>
                   </Center>
                   <br />
                   <MenuDivider />
