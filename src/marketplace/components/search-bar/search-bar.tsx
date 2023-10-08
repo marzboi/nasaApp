cc8a6bc5598156d5b1ba590e855a45c13fd40320
daasheo <jesusmudev@gmail.com>
2023-10-08 10:42:00 +0200

    Implement search bar.

--- src/marketplace/components/search-bar/search-bar.tsx
+++ src/marketplace/components/search-bar/search-bar.tsx
@@ -11,17 +11,17 @@ import { Search2Icon } from "@chakra-ui/icons";
 export const SearchBar = () => {
   return (
     <>
-      <InputGroup borderRadius={5} size="sm">
+      <InputGroup borderRadius={5} size="sm" >
         <InputLeftElement
           pointerEvents="none"
           children={<Search2Icon color="gray.600" />}
         />
-        <Input type="text" placeholder="Search..." border="1px solid #949494" />
+        <Input type="text" placeholder="Search..." borderLeftRadius={10} border="1px solid #949494" />
         <InputRightAddon
           p={0}
           border="none"
         >
-          <Button size="sm" borderLeftRadius={0} borderRightRadius={3.3} border="1px solid #949494">
+          <Button size="sm" borderLeftRadius={0} borderRightRadius={10} border="1px solid #949494">
             Search
           </Button>
         </InputRightAddon>
